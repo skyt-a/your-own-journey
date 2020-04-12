ee413c721278bd9e8e85d91c6b6b434eba1c62c2
RinGoku <yuyu80105@gmail.com>
2020-04-12 19:22:46 +0900

    WIP

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -4,6 +4,8 @@ import { createContext, Dispatch, useReducer } from 'react';
 import { reducer, StoreState, initialState } from '../reducers/reducer';
 import { Global, css } from '@emotion/core';
 import { Auth } from '../components/molecules/Auth';
+import styled from '@emotion/styled';
+import '../styles/style.scss';
 
 export const StoreContext = createContext<StoreState>(null as any);
 export const DispatchContext = createContext<Dispatch<any>>(null as any);
@@ -24,11 +26,21 @@ const App = (props: AppProps): any => {
             }
           `}
         />
-        <Auth {...props} />
+        <Main>
+          <Auth {...props} />
+        </Main>
       </DispatchContext.Provider>
     </StoreContext.Provider>
   );
 };
+
+const Main = styled.main`
+  max-width: 500px;
+  min-height: 100vh;
+  padding: 8px;
+  margin: auto;
+  background-color: #fff;
+`;
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
